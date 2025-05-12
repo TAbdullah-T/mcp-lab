b2fda874c57c7aed31d61ff905c6c07027c14206
Far-R <farzad.roozitalab@gmail.com>
2025-05-12 12:42:08 -0600

    update falcon

--- mcp_crash_course/src/external_tools_mcp_ollama.py
+++ mcp_crash_course/src/external_tools_mcp_ollama.py
@@ -1,11 +1,11 @@
 # You can find an interesting list of mcp servers here: https://github.com/punkpeye/awesome-mcp-servers
 from praisonaiagents import Agent, MCP
 
-# LLM choices: llama3.2 (3B), falcon3 (7B)
+# LLM choices: llama3.2 (3B), falcon3 (3B), falcon3:7b (7B)
 
 external_tool_agent = Agent(
     instructions="You are a helpful assistant with access to a tool.",
-    llm="ollama/falcon3",
+    llm="ollama/falcon3:7b",
     tools=MCP(
         "npx @openbnb/mcp-server-airbnb --ignore-robots-txt")
 )
