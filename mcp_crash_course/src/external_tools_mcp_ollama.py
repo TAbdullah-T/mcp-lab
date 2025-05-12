b14752d64390a179134c2e367fda6158c5bd290b
Farzad-R <farzad.roozitalab@gmail.com>
2025-05-11 19:51:14 -0600

    update external tool

--- mcp_crash_course/src/external_tools_mcp_ollama.py
+++ mcp_crash_course/src/external_tools_mcp_ollama.py
@@ -4,10 +4,10 @@ from praisonaiagents import Agent, MCP
 # LLM choices: llama3.2 (3B), falcon3 (7B)
 
 external_tool_agent = Agent(
-    instructions="You are a helpful assistant with access to a tool. Call it when the user asks for it.",
+    instructions="You are a helpful assistant with access to a tool.",
     llm="ollama/falcon3",
     tools=MCP(
         "npx @openbnb/mcp-server-airbnb --ignore-robots-txt")
 )
-user_input = "Find an apartment in Italy for 25 of September 2025."
+user_input = "Find a place in Italy for 20 of May and give me any result from the listing."
 response = external_tool_agent.start(user_input)
