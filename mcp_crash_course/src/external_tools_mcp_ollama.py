7840f72a442436e90e9c4515165c0d1bee6de264
Farzad-R <farzad.roozitalab@gmail.com>
2025-05-12 18:50:52 -0600

    update

--- mcp_crash_course/src/external_tools_mcp_ollama.py
+++ mcp_crash_course/src/external_tools_mcp_ollama.py
@@ -9,5 +9,5 @@ external_tool_agent = Agent(
     tools=MCP(
         "npx @openbnb/mcp-server-airbnb --ignore-robots-txt")
 )
-user_input = "Find an apartment in Italy for 25 of September 2025 for a single person."
+user_input = "Find a place in Italy for 20 of May and show me some options."
 response = external_tool_agent.start(user_input)
